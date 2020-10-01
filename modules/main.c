49d92a95d623524c66d1ef0ac9e5a44309c3a93a
settstep <ssette@gmx.de>
2020-10-01 15:17:22 +0200

    file main.c created

---
+++ modules/main.c
@@ -0,0 +1,12 @@
+/**
+ * \file    main.c
+ *
+ * \date    2020-10-01
+ *
+ * \author  settstep
+ *
+ * \section sec_main_c_history Historie
+ *
+ * \par     2020-10-01 settstep
+ * - file created
+ */
